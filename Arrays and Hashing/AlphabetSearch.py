84755b02e466a77b2b92186376ab0a678ac74456
Samaira Stewart <stewartsamaira@gmail.com>
2024-02-12 14:57:30 -0500

    Tested alphabetsearch and started merge sort

--- Arrays and Hashing/AlphabetSearch.py
+++ Arrays and Hashing/AlphabetSearch.py
@@ -1,6 +1,7 @@
 """Write a function that accepts a string that contains all the letters of the alphabet except one and returns the missing letter.
 The function should have a time complexity of O(N)"""
 
+
 # Approach:
 # Initialize an empty hashmap to store the occurrence of each letter.
 # Loop through the given string and update the hashmap with the count of each letter.
@@ -16,7 +17,7 @@ def find_missing_letter(s):
         alphabet_map[char] = alphabet_map.get(char, 0) + 1
 
     # Step 2: Check for the missing letter
-    for letter in "abcdefghijklmnopqrstuvwxyz":
+    for letter in "abcdefghijklmnopqrstuvwxyz":  
         if alphabet_map.get(letter, 0) == 0:
             return letter
 
